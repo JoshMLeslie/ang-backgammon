2148414dd752066ec1f701e6da7d7d61cf69e0b8
Josh Leslie <hello@joshmleslie.com>
2023-10-09 13:38:38 -0400

    cleanup naming conventions in util logic

--- src/app/util/logic.ts
+++ src/app/util/logic.ts
@@ -1,11 +1,11 @@
 import { ChitColumn } from '../interface/chit';
-import { Player } from '../interface/player';
+import { Player, PlayerColor } from '../interface/player';
 import { BoardCoord, BoardCoordMove } from '../interface/util';
 
 const movementDownhill = ({ from, to }: BoardCoordMove): boolean => {
   if (from.zone > to.zone) {
     return false;
-  } else if (from.zone === to.zone && from.element >= to.element) {
+  } else if (from.zone === to.zone && from.columnn >= to.columnn) {
     return false;
   }
   return true;
@@ -21,35 +21,35 @@ export const diceRanges = (dice: number[]): number[] => {
   return sums;
 };
 
-export const moveInDice = (
+export const moveWithinDice = (
   { from, to }: BoardCoordMove,
   dice: number[]
 ): boolean => {
   let dist: number;
   if (from.zone === to.zone) {
-    dist = to.element - from.element;
+    dist = to.columnn - from.columnn;
   } else {
-    dist = (to.zone + 1) * to.element - (from.zone + 1) * from.element;
+    dist = (to.zone + 1) * to.columnn - (from.zone + 1) * from.columnn;
   }
   return diceRanges(dice).some((d) => d <= dist) || false;
 };
 
 export const isValidMove = (
   chits: ChitColumn,
-  activePlayer: Player['color'],
+  activePlayer: PlayerColor,
   coords: BoardCoordMove,
   dice: number[]
 ) => {
   if (!movementDownhill(coords)) {
     return false;
   }
-  if (!moveInDice(coords, dice)) {
+  if (!moveWithinDice(coords, dice)) {
     return false;
   }
-  if (chits.count === 1 && chits.player.color !== activePlayer) {
+  if (chits.count === 1 && chits.color !== activePlayer) {
     return 'bounce';
   }
-  if (chits.count === 0 || chits.player.color === activePlayer) {
+  if (chits.count === 0 || chits.color === activePlayer) {
     return true;
   }
   return false;
